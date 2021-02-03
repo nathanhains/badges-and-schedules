2bd8267ea53ef54762d58750ea81bce0d1316926
nathan hains <nathanhains14@yahoo.com>
2021-02-03 15:25:17 -0500

    commit tree 0b92e41cdd8d7ef114da6e7691750fcd9d5b2a0c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    new_array = []
+    array.each do |name|
+        new_array << badge_maker(name)
+    end
+    new_array
+end
+
+def assign_rooms(speaker)
+    new_array = []
+    speaker.each do |speak|
+        new_array << "Hello, #{speak}! You'll be assigned to room #{(speaker.index(speak))+1}!"
+    end
+    new_array
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |attendee|
+        puts attendee
+    end
+    assign_rooms(attendees).each do |room|
+        puts room
+    end
+end
